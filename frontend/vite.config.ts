330dbad5819b27e8bec746bc46893ab61434c753
mlmc <mlmc3218@gmail.com>
2026-08-21 20:21:59 +0800

    fix: 配置修改

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const proxyTarget = env.VITE_PROXY_TARGET || 'http://127.0.0.1:8001'
-  const doodleProxyTarget = env.VITE_DOODLE_PROXY_TARGET || 'http://127.0.0.1:8001'
+  const proxyTarget = env.VITE_PROXY_TARGET || 'http://172.16.2.91:8000'
+  const doodleProxyTarget = env.VITE_DOODLE_PROXY_TARGET || 'http://172.16.2.91:8000'
   return {
     plugins: [react()],
     server: {
-      port: 5267,
+      port: 5173,
       proxy: {
         '/api/v1/doodles': {
           target: doodleProxyTarget,
